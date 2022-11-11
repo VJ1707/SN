ed45b6c609cd0513faf1ed3dd5dbec8d1900f24a
Your Name <you@example.com>
2022-11-11 20:50:08 +0530

    Updated code by me

--- src/main/java/com/helper/actionsclass/ActionHelper.java
+++ src/main/java/com/helper/actionsclass/ActionHelper.java
@@ -1,5 +1,15 @@
 package com.helper.actionsclass;
 
+import org.openqa.selenium.WebDriver;
+
 public class ActionHelper {
+	
+	private WebDriver driver;
+	
+	public void ActionsHelper(WebDriver driver) {
+		this.driver=driver;
+		
+	
+	}
 
 }
